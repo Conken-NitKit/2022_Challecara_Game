5ec4539ea75ee135b701dcd6781c8e547a03e679
woods-one <0820ricer@gmail.com>
2022-09-07 15:52:38 +0900

    追加:Youtubeのコメントを取得するアセットのインポート

---
+++ Assets/DownLoadAssets/YoutubeComment/Example/Example.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    [SerializeField]
+    GameObject sphere;
+
+    [SerializeField]
+    UnityEngine.UI.Text text;
+
+    List<string> commentList = new List<string>();
+
+    private void Start()
+    {
+        FindObjectOfType<YoutubeComment>().BeginGetComments();
+    }
+
+    public void OnComment(List<Comment> comments)
+    {
+        foreach(var c in comments)
+        {
+            commentList.Add(c.Message);
+        }
+
+        while(commentList.Count > 20)
+        {
+            commentList.RemoveAt(0);
+        }
+
+        text.text = "";
+        foreach (var c in commentList)
+        {
+            text.text += c + "\n";
+        }
+    }
+
+    public void OnSuperChat(Comment comment)
+    {
+        Debug.Log($"<color=red>{comment.SuperChatComment}->{comment.AmountDisplay}({comment.AmountMicros})</color>");
+
+        var go = Instantiate(sphere);
+        go.transform.position = Vector3.up * 10f;
+    }
+}
