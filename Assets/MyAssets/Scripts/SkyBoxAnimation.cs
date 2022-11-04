ab750a0a2b4754dfe899c685eb77260b6631c4d3
woods-one <0820ricer@gmail.com>
2022-11-05 00:32:40 +0900

    追加 : コメントアウトの追加,

--- Assets/MyAssets/Scripts/SkyBoxAnimation.cs
+++ Assets/MyAssets/Scripts/SkyBoxAnimation.cs
@@ -12,13 +12,16 @@ public class SkyBoxAnimation : MonoBehaviour
     
     private Material _skyboxMaterial;
 
+    // 最大の角度、変更することないと思うので定数化
+    private const float MaxAngle = 360f;
+
     void Start () {
         _skyboxMaterial = RenderSettings.skybox;
     }
 	
     void Update () {
-        //　スカイボックスマテリアルのRotationを操作して角度を変化させる
-        //SetFloatでMaterialの値を変更、Mathf.Repeatは規定値までいくと0に戻るメソッド
-        _skyboxMaterial.SetFloat("_Rotation", Mathf.Repeat(_skyboxMaterial.GetFloat("_Rotation") + rotateSpeed * Time.deltaTime, 360f));
+        // スカイボックスマテリアルのRotationを操作して角度を変化させる
+        // SetFloatでMaterialの値を変更、Mathf.Repeatは規定値までいくと0に戻るメソッド
+        _skyboxMaterial.SetFloat("_Rotation", Mathf.Repeat(_skyboxMaterial.GetFloat("_Rotation") + rotateSpeed * Time.deltaTime, MaxAngle));
     }
 }
