5996a16c559b9e1b6caf7ac997cbb256057ace02
Takayama <823ab172@gmail.com>
2022-11-11 21:11:09 +0900

    追加:最低限のメソッドのみ追加

--- Assets/MyAssets/Scripts/Enemy.cs
+++ Assets/MyAssets/Scripts/Enemy.cs
@@ -9,4 +9,19 @@ public abstract class Enemy : MonoBehaviour
     protected abstract float AttackPoint { get; set; }
     protected abstract float Velocity { get; set; }
     protected abstract float Score { get; set; }
+
+    protected void Attack()
+    {
+        Debug.Log($"基底クラスの記述です @{this.gameObject.name}.Attack");
+    }
+    
+    protected void AddDamage()
+    {
+        Debug.Log($"基底クラスの記述です @{this.gameObject.name}.AddDamage");
+    }
+    
+    protected void Dead()
+    {
+        Debug.Log($"基底クラスの記述です @{this.gameObject.name}.Dead");
+    }
 }
