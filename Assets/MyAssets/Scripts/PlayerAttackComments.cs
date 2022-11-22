dcbc30a12c51a2ac2da97625879c9d387af17272
NoguchiYuri <xiaonujinger@gmail.com>
2022-11-22 16:30:02 +0900

    変更： PlayerAttackCommentsスクリプト名

---
+++ Assets/MyAssets/Scripts/PlayerAttackComments.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// コメントを文字数によって変形させる基底クラス
+/// <summary>
+public class PlayerAttackComments : MonoBehaviour
+{
+
+}
