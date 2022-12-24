9cdf0c6834b23fc2830a13173bd1e051b9e6a59b
2rippe <xiaomujinger@gmail.com>
2022-12-25 08:07:52 +0900

    変更：Ranking画面に表示されるステータス

---
+++ Assets/MyAssets/Scripts/Enemy/RankingSort.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingSort : MonoBehaviour
+{
+    [SerializeField] Text easyRannkingText;
+    [SerializeField] Text easyRannkingScoreText;
+    [SerializeField] Text normalRanlinhText;
+    [SerializeField] Text normalRannkingScoreText;
+    [SerializeField] Text hardRainText;
+    [SerializeField] Text hardRannkingScoreText;
+    [SerializeField] RankingManager rankingManager;
+ 
+
+}
